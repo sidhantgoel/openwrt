37832936dbdf647b30a2920af5d70f782d9b9ab3
Sidhant Goel <sidhant@hexcod.in>
2025-09-23 14:04:48 +0000

    generate factory web ui image for be800
    Signed-off-by: Sidhant Goel <sidhant@hexcod.in>

--- target/linux/qualcommbe/image/ipq95xx.mk
+++ target/linux/qualcommbe/image/ipq95xx.mk
@@ -1,3 +1,5 @@
+DEVICE_VARS += TPLINK_SUPPORT_STRING
+
 define Device/8devices_kiwi-dvk
 	$(call Device/FitImage)
 	$(call Device/EmmcImage)
@@ -41,5 +43,8 @@ define Device/tplink_be800v1
 	KERNEL_SIZE := 6144k
 	SOC := ipq9574
 	DEVICE_PACKAGES := kmod-ath12k ath12k-firmware-qcn9274 f2fsck mkf2fs kmod-sfp
+	IMAGES += web-ui-factory.bin
+	IMAGE/web-ui-factory.bin := append-ubi | tplink-image-2023
+	TPLINK_SUPPORT_STRING := SupportList:\n{product_name:Archer BE800,product_ver:1.0.0,special_id:55530000}\n{product_name:Archer BE800,product_ver:1.0.0,special_id:43410000}\n{product_name:Archer BE800,product_ver:1.0.0,special_id:45550000}\n{product_name:Archer BE800,product_ver:1.0.0,special_id:484B0000}\n{product_name:Archer BE800,product_ver:1.0.0,special_id:41530000}\n{product_name:Archer BE800,product_ver:1.0.0,special_id:41550000}\n{product_name:Archer BE800,product_ver:1.0.0,special_id:53470000}\n{product_name:Archer BE800,product_ver:1.20,special_id:55530000}\n{product_name:Archer BE800,product_ver:1.20,special_id:45550000}\n{product_name:Archer BE800,product_ver:1.20,special_id:43410000}\n{product_name:Archer BE800,product_ver:1.20,special_id:41530000}\n{product_name:Archer BE800,product_ver:1.20,special_id:41550000}\n{product_name:Archer BE800,product_ver:1.20,special_id:53470000}\n
 endef
 TARGET_DEVICES += tplink_be800v1
